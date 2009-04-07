c1a07fed68b7c579325e47e8a8bd7e79ed590ec4
Rasmus Andersson <rasmus@notion.se>
2009-04-07 02:39:59 +0200

    updated the echo server example with documentation

--- examples/echo_server.c
+++ examples/echo_server.c
@@ -5,19 +5,30 @@
 #include "../src/event_base.h"
 #include "../src/tcp_server.h"
 #include "../src/tcp_client.h"
+#include "../src/mpool.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <err.h>
 #include <event.h>
 
+/**
+  We call this function when a new client have connected.
+*/
 static void on_client_connected(nt_tcp_client *client) {
-  printf("client %p connected from %s\n", client, nt_tcp_socket_host(client->socket));
-  // Send a hello message
-  static char data[] = "Send something to me and I will send it back to you my friend.\n";
+  printf("client %p connected from %s on port %d\n", client, 
+    nt_tcp_socket_host(client->socket), nt_tcp_socket_port(client->socket));
+  // Send a warm welcome
+  static char data[] = 
+    "Send something to me and I will send it back to you my friend.\n";
   bufferevent_write(client->bev, data, sizeof(data));
 }
 
+/**
+  Called when it's time to read something a client is sending to us.
+  
+  In this program we simply send back whetever we received.
+*/
 static void on_client_read(struct bufferevent *bev, nt_tcp_client *client) {
   /* 
     Simply write the bev->input to the buffer (bev->output), thus echoing
@@ -40,38 +51,83 @@ static void on_client_read(struct bufferevent *bev, nt_tcp_client *client) {
   */
 }
 
+/**
+  Handles connection "errors" (might EOF or a true error).
+  
+  Basically, this function is called when a client is no longer available.
+  The cause may be a channel error or a normal disconnect.
+*/
 static void on_client_error(struct bufferevent *bev, short what, nt_tcp_client *client) {
   if (what & EVBUFFER_EOF)
     printf("client %p disconnected\n", client);
   else
     warnx("client %p error\n", client);
-  nt_release(client);
+  nt_release(client); // Probably freeling the client, implying closing the
+                      // underlying socket.
 }
 
+/**
+  Accepts new connections.
+  
+  This function is called when a new client is knocking on the door, waiting
+  to be accept() ed. You have to accept (or remove) the event, or else we will
+  end up in an never-ending loop (because of nt_event_base).
+*/
 static void on_accept(int fd, short ev, nt_event_base_server *bs) {
-  printf("event: accept\n");
   nt_tcp_client *client;
   if ( (client = nt_tcp_client_accept(bs, fd, &on_client_read, NULL, &on_client_error)) )
     on_client_connected(client);
+  // In a real program, you would proabaly handle a NULL return from
+  // nt_tcp_client_accept here.
 }
 
 
 int main(int argc, char * const *argv) {
+  // Our server and base objects
   nt_tcp_server *server;
   nt_event_base *base;
-  char *addr = "";
-  int port = 8080, rc;
-  bool ipv6_enabled = true;
-  bool ipv6_only = false;
-  bool blocking = false;
   
-  // parse arguments
+  // Parameters written out for educational purposes
+  bool ipv6_enabled = true; // Enable binding to IP v6 addresses
+  bool ipv6_only = false;   // Only allow binding to IP v6 addresses
+  bool blocking = false;    // Use blocking I/O (not compatible with the
+                            // nt_event_base method).
+  
+  // Command line arguments, with default values
+  char *addr = "";  // Address to bind to, in human notion
+  int port = 7000;  // Port to bind to
+  
+  // Parse command line arguments
   if (argc > 1)
     addr = argv[1];
   if (argc > 2)
     port = atoi(argv[2]);
-
-  // ------------ server: ------------
+  
+  /** -----------------------------------------------------------------------
+    
+    Setup the global memory pool (optional).
+    
+    You can comment out this whole block in order to disable the library
+    global memory pool, which will cause the library to use standard malloc()
+    and friends.
+  */
+  
+  int error_p = 0;
+  nt_mpool_shared = nt_mpool_open(
+    0/* standard mpool */,
+    0/* system-default pagesize */,
+    NULL/* let mmap decide start address */,
+    &error_p/* mpool error pointer */);
+  if (!nt_mpool_shared) {
+    warnx("failed to open memory pool: %s -- disabling shared mpool",
+      nt_mpool_strerror(error_p));
+  }
+  
+  /** -----------------------------------------------------------------------
+    
+    Create and bind a TCP server.
+    
+  */
   
   // Create server
   server = nt_tcp_server_new(&on_accept);
@@ -90,7 +146,17 @@ int main(int argc, char * const *argv) {
       nt_tcp_socket_port(server->socket6));
   }
   
-  // ------------ base: ------------
+  /** -----------------------------------------------------------------------
+  
+    Create an event base, add the server and enter the runloop.
+    
+    A nt_event_base handles asynchronous I/O in the most efficient manner
+    possible (epoll, kqueue, etc).
+    
+    In a multi-threaded environment you have one event base per thread and
+    add the server to each event base.
+  
+  */
   
   base = nt_event_base_new();
   
@@ -99,11 +165,14 @@ int main(int argc, char * const *argv) {
     exit(1);
   
   // Enter event base runloop
-  rc = nt_event_base_loop(base, 0);
+  int rc = nt_event_base_loop(base, 0);
   if (rc < 0)
     warnx("nt_event_base_loop: error");
   else if (rc > 0)
     warnx("nt_event_base_loop: no events");
+  
+  
+  /** ----------------------------------------------------------------------- */
   
   // release our reference to base
   nt_release(base);
